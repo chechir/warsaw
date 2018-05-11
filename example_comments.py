0b4f5c9e218ed856e1713e0fc23db60a1af084f1
maggi <a@a.com>
2018-05-11 08:32:01 +0100

    added example comments

--- example_comments.py
+++ example_comments.py
@@ -1,10 +1,10 @@
+import re
 url = 'https://www.kaggle.com/lystdo/lstm-with-word2vec-embeddings'
 
 
 def get_wordlist_from_text(
         text, remove_stopwords=False, stem_words=False):
     text = convert_words_to_lower(text)
-    text = split_words(text)
     if remove_stopwords:
         text = apply_remove_stopwords(text)
     text = clean_text(text)
@@ -13,15 +13,27 @@ def get_wordlist_from_text(
     return text
 
 
-def convert_words_to_lower():
-    pass
+def convert_words_to_lower(text):
+    result = _split_words(text)
+    result = result.lower(result)
+    result = _join_words(result)
+    return result.lower()
 
 
-def split_words():
-    pass
-
-
-def clean_text():
+def clean_text(text):
+    result = text[:]
+    replaces = {
+            "[^A-Za-z0-9^,!.\/'+-=]": ' ',
+            ' e g ': ' eg ', "\'s": " ",
+            "\'ve": " have ", "can't": "cannot ",
+            "n't": " not ",
+            "i'm": "i am ",
+            "\'re": " are ",
+            "\'d": " would ",
+            "\'ll": " will "
+            }
+    for pattern, replace_value in replaces.iteritems():
+        result = re.sub(pattern, replace_value, result)
     pass
 
 
@@ -37,4 +49,10 @@ def apply_remove_stopwords():
     pass
 
 
+def _split_words(text):
+    return [word for word in text]
+
+
+def _join_words(text):
+    return ' '.join(text)
 
